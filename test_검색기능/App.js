e79e7b667c8cac56df2a7428a267ac537c554f94
duck <godqhr2256@gmail.com>
2023-02-18 23:50:00 +0900

    vanilaJS 검색기능 파일명 변경

---
+++ test_검색기능/App.js
@@ -0,0 +1,15 @@
+import SearchInput from "./SearchInput.js";
+
+export default function App({ $target }) {
+  this.state = {
+    fetchedLanguages: [],
+    selectedLanguages: [],
+  };
+  this.setState = (nextState) => {
+    // TODO : 구현해야함
+  };
+  const searchInput = new SearchInput({
+    $target,
+    initialState: "",
+  });
+}
