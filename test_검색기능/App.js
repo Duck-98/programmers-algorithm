0a7cbcb954ec364fd7424018d4f4e83a82315522
duck <godqhr2256@gmail.com>
2023-02-19 00:31:28 +0900

    vanilaJS 검색기능 [ 검색한 내용 클릭& 클릭한 내용 화면에 띄어주기  기능 ]

--- test_검색기능/App.js
+++ test_검색기능/App.js
@@ -1,4 +1,6 @@
+import { fetchLanguages } from "./api.js";
 import SearchInput from "./SearchInput.js";
+import Suggestion from "./Suggestion.js";
 
 export default function App({ $target }) {
   this.state = {
@@ -6,10 +8,55 @@ export default function App({ $target }) {
     selectedLanguages: [],
   };
   this.setState = (nextState) => {
-    // TODO : 구현해야함
+    this.state = {
+      ...this.state,
+      ...nextState,
+    };
+    suggestion.setState({
+      selectedIndex: 0,
+      items: this.state.fetchedLanguages,
+    });
   };
   const searchInput = new SearchInput({
     $target,
     initialState: "",
+    onChange: async (keyword) => {
+      if (keyword.length === 0) {
+        this.setState({
+          fetchedLanguages: [],
+        });
+      } else {
+        const languages = await fetchLanguages(keyword);
+        this.setState({
+          fetchedLanguages: languages,
+        });
+        console.log(languages);
+      }
+    },
+  });
+
+  const suggestion = new Suggestion({
+    $target,
+    initialState: {
+      selectedIndex: 0,
+      items: [],
+    },
+    onSelect: (language) => {
+      alert(language);
+      // 이미 선택된 언어라면 맨 뒤로 보내기
+      const nextSelectedLanguages = [...this.state.selectedLanguages];
+      const index = nextSelectedLanguages.findIndex(
+        (selectedLanguages) => selectedLanguages === language,
+      );
+      if (index > -1) {
+        nextSelectedLanguages.splice(index, 1);
+      }
+      nextSelectedLanguages.push(language);
+
+      this.setState({
+        ...this.state,
+        selectedLanguages: nextSelectedLanguages,
+      });
+    },
   });
 }
